6542aa4c8b9ecddac4fb293b429abbd07fa158d2
Rajda-Hyder <rajdahyder@gmail.com>
2025-03-16 14:37:01 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+Noun_Num = input("Enter a noun;number: ")
+Plural_noun = input("Enter a Plural Noun: ")
+Verb = input("Enter a Verb: ")
+Adj = input("Enter an Adjective: ")
+Noun_Num1 = input("Enter a noun;number: ")
+Noun_Place = input("Enter a noun;Place: ")
+Prop_noun = input("Enter a Proper Noun[First Name]: ")
+Prop_noun1 = input("Enter a Proper Noun[First Name]: ")
+Verb_ed = input("Enter a verb in -ed form: ")
+Noun = input("Enter a noun: ")
+Verb_ed1 = input("Enter a verb in -ed form: ")
+Noun_Num1 = input("Enter a noun;number: ")
+Adj1 = input("Enter an Adjective: ")
+Adj2 = input("Enter an Adjective: ")
+Verb_ed2 = input("Enter a verb in -ed form: ")
+Verb_ed3 = input("Enter a verb in -ed form: ")
+Noun1 = input("Enter a noun: ")
+Verb_ed4 = input("Enter a verb in -ed form: ")
+Plural_noun1 = input("Enter a Plural Noun: ")
+Verb1 = input("Enter a Verb: ")
+Adverb = input("Enter an Adverb: ")
+Noun_Num2 = input("Enter a noun;number: ")
+
+
+madlib = f"""On July {Noun_Num} 1969,two American {Plural_noun} were the first to {Verb} on the Moon.
+This {Adj} trip took {Noun_Num1} days to reach the moon from {Noun_Place} As {Prop_noun} Armstrong and
+{Prop_noun1} Aldrin {Verb_ed} on to the {Noun} of the moon, Armstrong {Verb_ed1} the famous words,
+'That's {Noun_Num1} {Adj1} step for a man,one {Adj2} leap for mankind'.Soon after,Aldrin {Verb_ed2} onto 
+the moon and together,they {Verb_ed3} a U.S {Noun1} on the surface.They {Verb_ed4} {Plural_noun1} from 
+the moon's surface to {Verb1} back to Earth and {Adverb} returned home {Noun_Num2} days later."""
+
+print (madlib)
